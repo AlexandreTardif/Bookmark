24872b7b29a6d37dbfe2db2e5cdab43abd622fb9
AlexandreTardif <74985691+TobyLeRoi@users.noreply.github.com>
2021-01-28 09:18:56 -0500

    Ajoutez des fichiers projet.

---
+++ Laboratoire1/Data/Laboratoire1Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Laboratoire1.Models;
+
+namespace Laboratoire1.Data
+{
+    public class Laboratoire1Context : DbContext
+    {
+        public Laboratoire1Context (DbContextOptions<Laboratoire1Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Laboratoire1.Models.Bookmark> Bookmark { get; set; }
+    }
+}
---
+++ Laboratoire1/Views/Bookmarks.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace Laboratoire1.Views
+{
+    public class Bookmarks
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        [Url]
+        public string Url { get; set; }
+    }
+}
